5792ef40a7060790cefbc681d81b9edb50b2040c
LollyJollyCoconut <84753028+LollyJollyCoconut@users.noreply.github.com>
2026-04-06 21:01:40 -0700

    e
    w

--- sketch.js
+++ sketch.js
@@ -7,16 +7,42 @@ let thiefImage1;
 let thiefImage2;
 let thief;
 let policeImages = [];
+let collectible;
 
 function preload() {
   backgroundImage = loadImage('Images/background.png');
   thiefImage1 = loadImage("Images/thief 1.png");
   thiefImage2 = loadImage("Images/thief 2.png");
+  policeOfficer1 = loadImage("Images/cop.png");
+  policeOfficer2 = loadImage("Images/glock.png");
+  policeOfficer3 = loadImage("Images/shotgun.png");
+  policeOfficer4 = loadImage("Images/tank.png");
+  policeOfficer5 = loadImage("Images/jet.png");
+  bullet1 = loadImage("Images/bullet.png");
+  bunchaBullets = loadImage("Images/bullets.png");
+  heart = loadImage("Images/heart.png");
+  bubble = loadImage("Images/bubble.png");
+  shield = loadImage("Images/shield.png");
+  speed = loadImage("Images/speed.png");
+  coin = loadImage("Images/coin.png");
+  money = loadImage("Images/$$$.png");
+  diamond = loadImage("Images/diamond.png");
+  greyCar = loadImage("Images/grey car.png");
+  redCar = loadImage("Images/red car.png");
+  blueCar = loadImage("Images/blue car.png");
+  bus = loadImage("Images/bus.png");
+  truck = loadImage("Images/truck.png");
+  bike = loadImage("Images/bike.png");
+  redGuy = loadImage("Images/red.png");
+  blackGuy = loadImage("Images/black.png");
+  greenGuy = loadImage("Images/green.png");
+  scooter = loadImage("Images/scooter.png");
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
   calculateBackgroundImageDimensions();
   thief = new Thief();
+  collectible = new Money();
 }
 
 function draw() {
@@ -33,6 +59,7 @@ function draw() {
   }
   thief.update();
   thief.show();
+  collectible.show()
 }
 
 function windowResized() {
@@ -87,5 +114,31 @@ class Thief {
     this.y += this.vy;
     this.vy += this.gravity;
     this.y = constrain(this.y, 0, this.groundY);
+  }
+}
+
+class Money {
+  constructor() {
+    this.spriteHeight = 50;
+    this.spriteWidth = 50;
+    this.x = width/2;
+    this.y = height/2;
+    this.currentImage = money;
+    this.type = "coin";
+  }
+  show() {
+    image(this.currentImage, this.x, this.y, this.spriteWidth, this.spriteHeight);
+  }
+  changeType() {
+    if (this.type == "coin") {
+      this.currentImage = coin;
+      this.spriteWidth = 60;
+      this.spriteHeight = 60;
+    }
+    else if (this.type == "money") {
+      this.currentImage = money;
+      tbis.spriteWidth = 50;
+      this.spriteHeight = 50;
+    }
   }
 }
